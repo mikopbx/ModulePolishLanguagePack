08ba9f928c32788b0bbc364c74cb0a6bceceddd5
Nikolay Beketov <nbek@miko.ru>
2026-04-06 14:12:05 +0700

    Initial commit: ModulePolishLanguagePack language pack for MikoPBX

---
+++ Messages/en/ModulePolishLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModulePolishLanguagePack' => 'Polish Language Pack',
+    'SubHeaderModulePolishLanguagePack' => 'Complete Polish language support for MikoPBX',
+];
---
+++ Messages/ru/ModulePolishLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModulePolishLanguagePack' => 'Языковой пакет - Польский',
+    'SubHeaderModulePolishLanguagePack' => 'Комплексная поддержка польского языка для системы MikoPBX',
+];
